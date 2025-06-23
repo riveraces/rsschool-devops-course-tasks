d855ffe3821d51634c231835891a677c54fc47da
cesarri <rivera_cesar@live.com.mx>
2025-06-23 16:20:25 -0500

    Fix Terraform formatting issues

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "crmterrformstate"
     key            = "terraform/state/terraform.tfstate"
-    region         = "us-east-2"        
+    region         = "us-east-2"
     dynamodb_table = "terraform-state-locks"
   }
 }
